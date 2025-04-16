c25fa11bf1662ef381bf9635fcf4fd6e73ba564f
monisri <monisrir95@gmail.com>
2025-04-16 08:23:14 +0000

    aiwins2

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-zhekzhe7sbl66 FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-zhekzhe7sbl66;
+ALTER ROLE db_datawriter ADD MEMBER id-zhekzhe7sbl66;
 
